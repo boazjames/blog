d280d4bb3a08bc307e4deacace45c7b4abb40cf7
Boaz James Otieno <boazjames2@yahoo.com>
2018-05-22 08:21:28 +0300

    Delete config.php

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'blog');
-
-$site_description="Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.";
